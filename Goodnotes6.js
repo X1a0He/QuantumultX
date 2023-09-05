6ffa9097dc91f7fdfcefbc94117eaa69036a5fc8
X1a0He <1171707139@qq.com>
2023-09-06 00:41:12 +0800

    Test fix Goodnotes6 again by X1a0He

--- Goodnotes6.js
+++ Goodnotes6.js
@@ -1,26 +1,41 @@
 let currentDate = new Date().toISOString();
 let X1a0He = {
+    "request_date_ms": 1693929600000,
+    "request_date": "2023-09-05T16:00:00.000Z",
     "subscriber": {
-        "subscriptions": {
+        "non_subscriptions": {
             "com.goodnotes6.one_time_unlock_3999": [
                 {
+                    "is_sandbox": false,
+                    "ownership_type": "PURCHASED",
+                    "id": "46ab585dbc",
                     "original_purchase_date": currentDate,
+                    "store_transaction_id": "300001271068792",
                     "purchase_date": currentDate,
-                    "store": "app_store",
-                    "ownership_type": "PURCHASED"
+                    "store": "app_store"
                 }
             ]
         },
+        "first_seen": currentDate,
+        "other_purchases": {
+            "com.goodnotes6.one_time_unlock_3999": {
+                "purchase_date": currentDate
+            }
+        },
+        "management_url": null,
+        "subscriptions": {},
         "entitlements": {
             "apple_access": {
+                "grace_period_expires_date": null,
                 "purchase_date": currentDate,
-                "product_identifier": "com.goodnotes6.one_time_unlock_3999"
+                "product_identifier": "com.goodnotes6.one_time_unlock",
+                "expires_date": null
             },
-            "crossplatform_access": {
-                "purchase_date": currentDate,
-                "product_identifier": "com.goodnotes6.one_time_unlock_3999"
-            }
-        }
+            "crossplatform_access": {}
+        },
+        "original_purchase_date": currentDate,
+        "last_seen": currentDate,
+        "Warning": ""
     }
 };
 $done({ body: JSON.stringify(X1a0He) });
