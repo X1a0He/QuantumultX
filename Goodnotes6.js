aff12d1342ea7b5944ccf9c0056447a78ffaea24
X1a0He <1171707139@qq.com>
2023-09-06 00:49:19 +0800

    Fuck you Goodnotes6

--- Goodnotes6.js
+++ Goodnotes6.js
@@ -1,5 +1,7 @@
 let currentDate = new Date().toISOString();
 let X1a0He = {
+    "request_date_ms": 1691760087616,
+    "request_date": "2023-08-11T13:21:27Z",
     "subscriber": {
         "non_subscriptions": {
             "com.goodnotes6.one_time_unlock": [
@@ -7,16 +9,18 @@ let X1a0He = {
                     "is_sandbox": false,
                     "ownership_type": "PURCHASED",
                     "id": "46ab585dbc",
-                    "original_purchase_date": currentDate,
+                    "original_purchase_date": "2022-11-10T23:58:09Z",
                     "store_transaction_id": "300001271068792",
-                    "purchase_date": currentDate,
+                    "purchase_date": "2023-08-10T23:58:09Z",
                     "store": "app_store"
                 }
             ]
         },
+        "first_seen": "2023-08-09T13:55:38Z",
+        "original_application_version": "1578030.383984727",
         "other_purchases": {
             "com.goodnotes6.one_time_unlock": {
-                "purchase_date": currentDate
+                "purchase_date": "2023-08-10T23:58:09Z"
             }
         },
         "management_url": null,
@@ -24,13 +28,14 @@ let X1a0He = {
         "entitlements": {
             "apple_access": {
                 "grace_period_expires_date": null,
-                "purchase_date": currentDate,
+                "purchase_date": "2023-08-10T14:16:41Z",
                 "product_identifier": "com.goodnotes6.one_time_unlock",
                 "expires_date": null
-            },
-            "crossplatform_access": {}
+            }
         },
-        "original_purchase_date": currentDate
+        "original_purchase_date": "2022-04-05T10:43:53Z",
+        "original_app_user_id": "1d6316b8-aab7-4c1f-9dee-df471814b03e",
+        "last_seen": "2023-08-11T13:18:22Z"
     }
 };
 $done({ body: JSON.stringify(X1a0He) });
